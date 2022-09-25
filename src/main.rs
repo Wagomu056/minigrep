7ab8d393e7c64be220b40a0e2c799d40693dac52
Takuya Azumaguchi <catchballer@gmail.com>
2022-09-25 19:58:46 +0900

    Add lib module

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In fine {}", config.filepath);
 
-    println!("Searching for {}", query);
-    println!("In fine {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
