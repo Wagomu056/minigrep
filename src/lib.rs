153ba6a05abedfb272bec04241b42c87b46ee789
Takuya Azumaguchi <catchballer@gmail.com>
2022-09-26 22:12:00 +0900

    Add test of Config

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,57 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn config_arg0() {
+        let args: Vec<String> = vec![];
+        Config::new(&args).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn config_arg1() {
+        let self_str = String::from("lib.rs");
+        let args: Vec<String> = vec![self_str];
+        Config::new(&args).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn config_arg2() {
+        let self_str = String::from("lib.rs");
+        let word_str = String::from("word");
+        let args: Vec<String> = vec![self_str, word_str];
+        Config::new(&args).unwrap();
+    }
+
+    #[test]
+    fn config_arg3() {
+        let self_str = String::from("lib.rs");
+        let word_str = String::from("word");
+        let file_str = String::from("tests/test.txt");
+        let args: Vec<String> = vec![self_str, word_str, file_str];
+
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, args[1]);
+        assert_eq!(config.filepath, args[2]);
+    }
+
+    #[test]
+    fn config_arg4() {
+        let self_str = String::from("lib.rs");
+        let word_str = String::from("word");
+        let file_str = String::from("tests/test.txt");
+        let append_str = String::from("append");
+        let args: Vec<String> = vec![self_str, word_str, file_str, append_str];
+
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, args[1]);
+        assert_eq!(config.filepath, args[2]);
+    }
+}
